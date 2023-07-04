788c140cc809e47996b0933ca63b086d4ab9ff06
AnastasiaDonets <110225975+AnastasiaDonets@users.noreply.github.com>
2023-07-04 21:08:32 +0000

    Deploying to gh-pages from @ AnastasiaDonets/goit-react-hw-08-phonebook@e8314683a48d4ca782a616832011582fa345d4ef 🚀

---
+++ static/js/828.e13b71f4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[828],{828:function(t,e,r){r.d(e,{Ix7:function(){return c}});var a=r(9983);function c(t){return(0,a.w_)({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M10.293 11.707l-3.293-3.293v-4.414h2v3.586l2.707 2.707zM8 0c-4.418 0-8 3.582-8 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zM8 14c-3.314 0-6-2.686-6-6s2.686-6 6-6c3.314 0 6 2.686 6 6s-2.686 6-6 6z"}}]})(t)}}}]);
+//# sourceMappingURL=828.e13b71f4.chunk.js.map
