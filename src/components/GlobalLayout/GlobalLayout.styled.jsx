c02414e302476d04c36082ee08ea324dc0f5c34c
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-07-05 00:12:29 +0300

    height update

--- src/components/GlobalLayout/GlobalLayout.styled.jsx
+++ src/components/GlobalLayout/GlobalLayout.styled.jsx
@@ -4,8 +4,9 @@ import background from '../../images/phone-2.jpg';
 
 export const MainContainer = styled.div`
   width: 100%;
+  height: 1200px;
   background-image: url(${background});
-  background-size: 100% auto;
+  background-size: 100% 1200px;
   background-repeat: no-repeat;
 `;
 
